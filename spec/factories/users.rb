5689480431c2c509bceab419092abf11907d5035
RavishankarArya <rshankarg@qwinixtech.com>
2015-08-04 11:27:10 +0530

    first commit

---
+++ spec/factories/users.rb
@@ -0,0 +1,11 @@
+FactoryGirl.define do
+  factory :user do
+    first_name "MyString"
+last_name "MyString"
+email "MyString"
+password_digest "MyString"
+phone "MyString"
+auth_token "MyString"
+  end
+
+end
